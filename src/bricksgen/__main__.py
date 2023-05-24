b99acb1574d1edc90387a96963baa96e203f46d2
Ed Sweeney <ed@onextent.com>
2023-05-24 10:35:47 -0700

    stubbed

--- src/bricksgen/__main__.py
+++ src/bricksgen/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-gen navactor json for testing the navactor
+gen simulated IOT observations from a bricks schema
 """
 import argparse
 
@@ -8,15 +8,16 @@ def main():
     """
     entry point
     """
-    parser = argparse.ArgumentParser(description='Generate Observations in Bricks format')
+    parser = argparse.ArgumentParser(description='Generate from bricks')
     parser.add_argument('--iterations', type=int, default=3,
-                        help='Number of iterations to report on boat navigation')
+                        help='Number of iterations to report')
     parser.add_argument('--interval-dur', type=int, default=1,
                         help='Minutes between iterations and reports')
     parser.add_argument('--hours-ago', type=int, default=12,
                         help='Initial timestamp for each generated observations is HOURS_AGO hours')
     parser.add_argument('--compact-json', action='store_true', help='Generate Compact JSON')
     args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
